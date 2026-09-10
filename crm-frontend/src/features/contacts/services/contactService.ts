eb6327cd71d79897558ef71d7b38410a2e321863
Fernando Garcia Tejeda <nando.garcia2504@gmail.com>
2026-09-09 21:58:11 -0600

    Update contactService.ts

--- crm-frontend/src/features/contacts/services/contactService.ts
+++ crm-frontend/src/features/contacts/services/contactService.ts
@@ -1,6 +1,6 @@
 import type { Contact, CreateContactData, UpdateContactData, GetContactResponse, GetContactsResponse, UpdateContactResponse, CreateContactResponse, GetAllContactsResponse } from "../types/contact.types";
 
-const API_URL = "https://crmprod-70ae5fa5478a.herokuapp.com/api/documents";
+const API_URL = "https://crmprod-70ae5fa5478a.herokuapp.com/api/contacts";
 
 export const getAllContacts = async(businessId: string): Promise<Contact[]> => {
 
